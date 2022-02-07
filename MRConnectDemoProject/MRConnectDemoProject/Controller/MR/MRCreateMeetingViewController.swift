4011efe497429221251124a291cedb6d3eb6da3f
anshbangia-docquity <ansh.bangia@docquity.com>
2022-02-07 11:51:38 +0530

    Create Meeting Bottom View

--- MRConnectDemoProject/MRConnectDemoProject/Controller/MR/MRCreateMeetingViewController.swift
+++ MRConnectDemoProject/MRConnectDemoProject/Controller/MR/MRCreateMeetingViewController.swift
@@ -11,15 +11,22 @@ class MRCreateMeetingViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var createButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = MyStrings.createMeeting
         cancelButton.setTitle(MyStrings.cancel, for: .normal)
+        createButton.setTitle(MyStrings.create.uppercased(), for: .normal)
     }
     
     @IBAction func cancelTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createTapped(_ sender: UIButton) {
+        print("Create Tapped")
         dismiss(animated: true, completion: nil)
     }
     
